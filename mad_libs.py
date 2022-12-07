965ebf84a2b51ef61727220167a96d482875a140
paulaa1297 <113406590+paulaa1297@users.noreply.github.com>
2022-12-07 13:18:19 -0500

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,8 @@
+adj= input ("Ingrese un adjetivo: ")
+verbo1= input ("Ingrese un verbo: ")
+verbo2= input ("Ingrese otro verbo: ")
+sustantivo_plural= input ("Ingrese un sustantivo en plural: ")
+
+madlib= (f"Programar es tan {adj}. Siempre me emociona porque me encanta {verbo1}. Aprende a {verbo2} con FreeCodeCamp y alcanza tus {sustantivo_plural}")
+
+print (madlib)
